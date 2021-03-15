b623b15db98b57bbd6680ac4ee3a6fde2a0151a9
木村毅樹 <80314997+takeki-kimura@users.noreply.github.com>
2021-03-15 15:41:00 +0900

    プルダウンの実装

--- app/models/room_user.rb
+++ app/models/room_user.rb
@@ -1,18 +1,4 @@
 class RoomUser < ApplicationRecord
-  belong_to :room
-  belong_to :user
-end
-
-  def change
-    create_table :rooms_user do |t|
-      t.references :room, foreign_key: true
-      t.references :user, foreign_key: true
-      t.timestanps
-    end
-  end
-end
-
-class Room < ApplicationRecord
-  has_many :rooms_users
-  has_many :users, through: :rooms_users
+  belongs_to :room
+  belongs_to :user
 end
